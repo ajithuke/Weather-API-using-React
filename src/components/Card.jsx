c36af4e25cdb81c7501676b623ddf2f8135e47b3
Ajit Huke <ajithuke63@gmail.com>
2024-02-21 09:46:29 +0530

    created components

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = ()=>{
+    return (
+        <div className="card">
+            <h2>Temperture</h2>
+            <h5>Location</h5>
+        </div>
+    )
+}
